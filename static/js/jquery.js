cda2f2a235e895a201dc933cac15fd2deda0ef47
volk <matiasdylanc@gmail.com>
2019-08-14 22:45:56 -0300

    CSFR added

---
+++ static/js/jquery.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    function ajax_login(){
+        $.ajax({
+            url: '/ajax-login',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function (response) {
+                console.log(response)
+            }
+        });
+    }
+
+    $("#login-form").submit(function(event){
+        event.preventDefault();
+        ajax_login();
+    })
+
+});
